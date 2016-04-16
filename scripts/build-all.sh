17574b5a1701820d3007c0d6342ad8168a4fca82
Marcus Müller <marcus@hostalia.de>
2016-04-16 16:36:21 +0200

    Made build-all script parametric

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -5,45 +5,34 @@ if ! bitbake parted-native mtools-native dosfstools-native; then
 	exit 1
 fi
 
-for MACHINE in ettus-e3xx-sg1 ettus-e3xx-sg3; do
+machines="ettus-e3xx-sg1 ettus-e3xx-sg3"
+flavours="gnuradio-dev gnuradio-demo"
+for MACHINE in $machines; do
 
-export MACHINE
-echo $MACHINE
+	export MACHINE
+	echo $MACHINE
 
-if ! bitbake gnuradio-dev-image; then
-	echo dev image build failed
-	exit 1
-fi
+	for FLAVOUR in $flavours;do
+		if ! bitbake $FLAVOUR-image; then
+			echo $FLAVOUR image build failed
+			exit 1
+		fi
 
-rm -rf images/$MACHINE/build
-if ! wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e gnuradio-dev-image -o images/$MACHINE; then 
-	echo dev image sd card build failed
-	exit 1
-fi
+		rm -rf images/$MACHINE/build
+		if ! wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e $FLAVOUR-image -o images/$MACHINE; then 
+			echo $FLAVOUR image sd card build failed
+			exit 1
+		fi
 
-export IMAGE_NAME=`ls images/$MACHINE/build/sdimage*mmcblk.direct`
-xz -T 4 $IMAGE_NAME
-mv $IMAGE_NAME.xz images/$MACHINE/sdimage-gnuradio-dev.direct.xz
-md5sum images/$MACHINE/sdimage-gnuradio-dev.direct.xz > images/$MACHINE/sdimage-gnuradio-dev.direct.xz.md5
+		export IMAGE_NAME=`ls images/$MACHINE/build/sdimage*mmcblk.direct`
+		xz -T 4 $IMAGE_NAME
+		mv $IMAGE_NAME.xz images/$MACHINE/sdimage-$FLAVOUR.direct.xz
+		md5sum images/$MACHINE/sdimage-$FLAVOUR.direct.xz > images/$MACHINE/sdimage-$FLAVOUR.direct.xz.md5
+	done
 
-if ! bitbake gnuradio-demo-image; then
-	echo demo image build failed
-	exit 1
-fi
-
-rm -rf images/$MACHINE/build
-if ! wic create ../meta-sdr/contrib/wks/sdimage-8G.wks  -e gnuradio-demo-image -o images/$MACHINE; then
-	echo demo image sd card build failed
-	exit 1
-fi
-
-export IMAGE_NAME=`ls images/$MACHINE/build/sdimage*mmcblk.direct`
-xz -T 4 $IMAGE_NAME
-mv $IMAGE_NAME.xz images/$MACHINE/sdimage-gnuradio-demo.direct.xz
-md5sum images/$MACHINE/sdimage-gnuradio-demo.direct.xz > images/$MACHINE/sdimage-gnuradio-demo.direct.xz.md5
 
 done
 
-bitbake -c populate_sdk gnuradio-dev-image
+bitbake -c populate_sdk $FLAVOUR-image
 
 
