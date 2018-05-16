EttusResearch/meta-ettus

commit 29b708293148aed9d59493963a3cacb9404c2825

meta-sulfur: gpsd-machine-conf: Add machine configuration
Add ni-sulfur GPSD machine configuration files. These contain mainly
hooks to actually turn on the GPSDO, as well as setting the correct
baudrate.

Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

meta-sulfur/recipes-navigation/gpsd/gpsd-machine-conf_1.0.bbappend
@@ -1,0 +1,25 @@
+FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+SRC_URI_ni-sulfur = " \
+    file://gpsd-machine \
+    file://device-hook \
+"
+
+inherit update-alternatives
+
+ALTERNATIVE_${PN}_ni-sulfur = "gpsd-defaults"
+ALTERNATIVE_LINK_NAME[gpsd-defaults] = "${sysconfdir}/default/gpsd"
+ALTERNATIVE_TARGET[gpsd-defaults] = "${sysconfdir}/default/gpsd.machine"
+ALTERNATIVE_PRIORITY[gpsd-defaults] = "100"
+COMPATIBLE_MACHINE = "ni-sulfur-rev6|ni-sulfur-rev5|ni-sulfur-rev4|ni-sulfur-rev3"
+
+RREPLACES_${PN} += "gpsd-conf"
+
+do_install() {
+    install -d ${D}/${sysconfdir}/default
+    install -m 0644 ${WORKDIR}/gpsd-machine ${D}/${sysconfdir}/default/gpsd.machine
+    install -d ${D}${sysconfdir}/gpsd/
+    install -m 0755 ${WORKDIR}/device-hook ${D}/${sysconfdir}/gpsd/device-hook
+}

meta-sulfur/recipes-navigation/gpsd/files/device-hook
@@ -1,0 +1,3 @@
+if [ "$2" = "ACTIVATE" ];then
+    stty -F /dev/ttyPS1 38400
+fi

meta-sulfur/recipes-navigation/gpsd/files/gpsd-machine
@@ -1,0 +1,5 @@
+# If you must specify a non-NMEA driver, uncomment and modify the next line
+GPSD_SOCKET="/var/run/gpsd.sock"
+GPSD_OPTIONS="-n"
+GPS_DEVICES="/dev/ttyPS1"
+

commit 7b7ed3b7b49f1c5535b65175f9cfbe822eaec31b

meta-sulfur: gpsd-machine-conf: Fix machine configuration
Fix machine configuration recipe for sulfur such that it
only installs the files when built for the ni-sulfur machines.

Fixes: 29b708293148 ("meta-sulfur: gpsd-machine-conf: Add machine configuration")
Signed-off-by: Sugandha Gupta <sugandha.gupta@ettus.com>
Signed-off-by: Moritz Fischer <moritz.fischer@ettus.com>

meta-sulfur/recipes-navigation/gpsd/gpsd-machine-conf_1.0.bbappend
@@ -17,7 +17,7 @@ COMPATIBLE_MACHINE = "ni-sulfur-rev6|ni-sulfur-rev5|ni-sulfur-rev4|ni-sulfur-rev
 
 RREPLACES_${PN} += "gpsd-conf"
 
-do_install() {
+do_install_ni-sulfur() {
     install -d ${D}/${sysconfdir}/default
     install -m 0644 ${WORKDIR}/gpsd-machine ${D}/${sysconfdir}/default/gpsd.machine
     install -d ${D}${sysconfdir}/gpsd/