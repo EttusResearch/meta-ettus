3a11d6579b2eefff6265dab87445bc8829f36770
Jörg Hofrichter <joerg.hofrichter@emerson.com>
2026-07-10 15:39:51 +0200

    Revert "contrib: usrp_emb: add 120s timeout to boot"
    This reverts commit 5d4b3f373f300681a59564be0d989023c0bc083c.

--- contrib/test/usrp_emb/usrp_emb/uboot.py
+++ contrib/test/usrp_emb/usrp_emb/uboot.py
@@ -27,7 +27,7 @@ class Uboot:
 
     def boot(self):
         self.uart.sendline("boot")
-        self.uart.expect("Starting kernel ...", timeout=120)
+        self.uart.expect("Starting kernel ...")
 
     def run_dhcp(self):
         self.uart.sendline("dhcp")
@@ -57,7 +57,7 @@ class Uboot:
     def bootm(self, addr, conf):
         addr_hex = hex(addr)
         self.uart.sendline(f"bootm {addr_hex}{conf}")
-        self.uart.expect("Starting kernel ...", timeout=120)
+        self.uart.expect("Starting kernel ...")
 
 
 class X4xxUboot(Uboot):
